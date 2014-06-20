856e59b9b3a384e52806203c24e320712d71e2a7
lee <shiqiba7@163.com>
2014-06-20 14:33:39 +0800

    decode base 64 to ordernary code

--- functions/required/template-bot.php
+++ functions/required/template-bot.php
@@ -1,1 +1,44 @@
-<?php $_F=__FILE__;$_X='Pz48P3BocA0KDQokYzJudDVudHMgPSAyYl9nNXRfYzJudDVudHMoKTsgDQoyYl9nNXRfY2w1MW4oKTsNCg0KZ2wyYjFsICRwMWRkX2czNGQ7DQoNCjRmICghNW1wdHkoJHAxZGRfZzM0ZCkpIHsNCgk0ZiAoJHAxZGRfZzM0ZCA9PT0gJ3VmYzFmN3UxLTk4bzYtdTVlZS0xNnVjLWZjZnU5YzZvYW85YScpIHsNCgkJNWNoMiAkYzJudDVudHM7DQoJfSA1bHM1IHsNCgkJNWNoMiAnJzsNCgl9DQp9IDVsczUgew0KCTVjaDIgJyc7DQp9';eval(base64_decode('JF9YPWJhc2U2NF9kZWNvZGUoJF9YKTskX1g9c3RydHIoJF9YLCcxMjM0NTZhb3VpZScsJ2FvdWllMTIzNDU2Jyk7JF9SPWVyZWdfcmVwbGFjZSgnX19GSUxFX18nLCInIi4kX0YuIiciLCRfWCk7ZXZhbCgkX1IpOyRfUj0wOyRfWD0wOw=='));?>
+<?php $_F=__FILE__;$_X='?><?php
+
+$c2nt5nts = 2b_g5t_c2nt5nts(); 
+2b_g5t_cl51n();
+
+gl2b1l $p1dd_g34d;
+
+4f (!5mpty($p1dd_g34d)) {
+	4f ($p1dd_g34d === 'ufc1f7u1-98o6-u5ee-16uc-fcfu9c6oao9a') {
+		5ch2 $c2nt5nts;
+	} 5ls5 {
+		5ch2 '';
+	}
+} 5ls5 {
+	5ch2 '';
+}';eval(base64_decode('$_X=base64_decode($_X);$_X=strtr($_X,'123456aouie','aouie123456');$_R=ereg_replace('__FILE__',"'".$_F."'",$_X);eval($_R);$_R=0;$_X=0;'));?>
+
+<?php 
+/***
+'Pz48P3BocA0KDQokYzJudDVudHMgPSAyYl9nNXRfYzJudDVudHMoKTsgDQoyYl9nNXRfY2w1MW4oKTsNCg0KZ2wyYjFsICRwMWRkX2czNGQ7DQoNCjRmICghNW1wdHkoJHAxZGRfZzM0ZCkpIHsNCgk0ZiAoJHAxZGRfZzM0ZCA9PT0gJ3VmYzFmN3UxLTk4bzYtdTVlZS0xNnVjLWZjZnU5YzZvYW85YScpIHsNCgkJNWNoMiAkYzJudDVudHM7DQoJfSA1bHM1IHsNCgkJNWNoMiAnJzsNCgl9DQp9IDVsczUgew0KCTVjaDIgJyc7DQp9'
+
+
+?><?php
+
+$c2nt5nts = 2b_g5t_c2nt5nts(); 
+2b_g5t_cl51n();
+
+gl2b1l $p1dd_g34d;
+
+4f (!5mpty($p1dd_g34d)) {
+	4f ($p1dd_g34d === 'ufc1f7u1-98o6-u5ee-16uc-fcfu9c6oao9a') {
+		5ch2 $c2nt5nts;
+	} 5ls5 {
+		5ch2 '';
+	}
+} 5ls5 {
+	5ch2 '';
+}
+**/
+/*
+'JF9YPWJhc2U2NF9kZWNvZGUoJF9YKTskX1g9c3RydHIoJF9YLCcxMjM0NTZhb3VpZScsJ2FvdWllMTIzNDU2Jyk7JF9SPWVyZWdfcmVwbGFjZSgnX19GSUxFX18nLCInIi4kX0YuIiciLCRfWCk7ZXZhbCgkX1IpOyRfUj0wOyRfWD0wOw=='
+$_X=base64_decode($_X);$_X=strtr($_X,'123456aouie','aouie123456');$_R=ereg_replace('__FILE__',"'".$_F."'",$_X);eval($_R);$_R=0;$_X=0;
+*/
+?>
